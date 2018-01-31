55d221eda17588dda9baf412aa2c69e6af70cb4e
Clinton Mead <clintonmead@gmail.com>
2018-01-31 20:42:43 +1100

    Document incomplete Compat module

--- src/Freelude/Impl/Compat.hs
+++ src/Freelude/Impl/Compat.hs
@@ -8,29 +8,26 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE TypeInType #-}
 
+{-|
+This module isn't really useful yet, see the following [issue](https://github.com/clintonmead/freelude/issues/1)
+-}
 module Freelude.Impl.Compat (
   ) where
 
+
 import Prelude hiding (Functor, fmap, (<$>))
-import Data.Kind (Type)
 
 import qualified Freelude.Impl.Classes as Class
 import Freelude.Impl.Classes (
   FunctionP,
-  BasicFunctorP,
-  FunctorT,
-  fmap,
-  (<$>),
-  FunctorSrcC, FunctorDstC
+  BasicFunctorP
+--  FunctorT,
+--  fmap,
+--  FunctorSrcC, FunctorDstC
   )
 
 class    Class.Functor FunctionP (BasicFunctorP f) => Functor f
 instance Class.Functor FunctionP (BasicFunctorP f) => Functor f
-
-
-
-
-
 
 --class    Class.Functor FunctionP (SimpleFunctorP src_c dst_c f) => UnrestrictedFunctor f
 --instance Class.Functor FunctionP (SimpleFunctorP src_c dst_c f) => UnrestrictedFunctor f
